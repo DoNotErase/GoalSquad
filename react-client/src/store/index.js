348692fdcaf9999dac5fec06ea3228b8654da702
Jacob Granberry <jacobgranberry@gmail.com>
2018-03-15 11:12:30 -0500

    stubs out store

--- react-client/src/store/index.js
+++ react-client/src/store/index.js
@@ -1,3 +1,13 @@
 import { createStore } from 'redux';
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+
+const reducer = combineReducers({  //combines reducers from reducer folder
+    data: mainReducer,
+    routing: routerReducer
+});
+
+//creates store and allows for chrome redux plugin
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export default store;
