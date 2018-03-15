2162a5159c8bfb2dd0017afe7c12ffa76d86fb66
ShahzebMeghani <shahzebmeghani@gmail.com>
2018-03-15 18:08:57 -0500

    add sql connection beginnings

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,7 +1,11 @@
-var mysql = require('mysql');
-var config = require('../config');
+const mysql = require('mysql');
+const config = require('../config');
+const Promise = require('bluebird');
+//needed for mysql
+Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+Promise.promisifyAll(require("mysql/lib/Pool").prototype);
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: config.aws.RDS_HOSTNAME,
   user: config.aws.RDS_USERNAME,
   password: config.aws.RDS_PASSWORD,
@@ -9,21 +13,5 @@ var connection = mysql.createConnection({
   database : 'goalsquad'
 });
 
-// var connection = mysql.createConnection({
-//   host: process.env.RDS_HOSTNAME,
-//   user: process.env.RDS_USERNAME,
-//   password: process.env.RDS_PASSWORD,
-//   port: process.env.RDS_PORT
-// });
-
-var selectAll = function(callback) {
-  connection.query('SELECT * FROM items', function(err, results, fields) {
-    if(err) {
-      callback(err, null);
-    } else {
-      callback(null, results);
-    }
-  });
-};
 
 module.exports.selectAll = selectAll;
