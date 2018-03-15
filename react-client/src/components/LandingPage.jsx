c828363731fa1122634a63905b0351ec522c26b6
Marci McClenon <mccmarci@gmail.com>
2018-03-15 15:27:45 -0500

    integrating redux implementation

--- react-client/src/components/LandingPage.jsx
+++ react-client/src/components/LandingPage.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import axios from 'axios';
 
 const LandingPage = (props) => {
+    
+    let getData = function() {
+        console.log('click')
+        axios('/fitbit/lifetime')
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
     return (
-        <div>
-            "Hello World"
-        </div>
+    <div>
+        <p> hello world </p>
+        <a href="http://127.0.0.1:3000/auth/fitbit"><button> Connect to Fitbit </button></a>
+
+        <button onClick={getData}> getLifeTimeData </button>
+    </div> 
     ) 
 }
 
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,48 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import List from './components/List.jsx';
-import axios from 'axios'
+import { render } from 'react-dom';
+import Root from './components/Root.jsx';
+import store from './store/index.js';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      user: ''
-    }
-  }
-
-  componentWillMount() {
-    axios('/user')
-      .then(res => {
-        this.setState({user: res.data.user});
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  }
-
-  getData() {
-    console.log('click')
-    axios('/fitbit/lifetime')
-      .then(res => {
-        console.log(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  }
-
-  render () {
-    return (<div>
-      <p>Hello {this.state.user}</p>
-
-        <a href="http://127.0.0.1:3000/auth/fitbit"><button> Connect to Fitbit </button></a>
-        <button onClick = {this.getData}> getLifeTimeData </button>
-      </div>
-    )
- 
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+render(
+  <Root store={store} />,
+  document.getElementById('app')
+);
