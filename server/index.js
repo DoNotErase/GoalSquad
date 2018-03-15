2716ac4cca13c15c9ed79496e58c4ec9560a4c82
Marci McClenon <mccmarci@gmail.com>
2018-03-15 18:22:47 -0500

    placating the linter

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const session = require('express-session');
 const FitbitStrategy = require('passport-fitbit-oauth2').FitbitOAuth2Strategy;
 const passport = require('passport');
 const config = require('../config.js');
-
 const axios = require('axios');
 
 const app = express();
@@ -31,47 +30,44 @@ app.use(passport.session({
 // }
 // app.use(logger);
 
-let globalProfile;
-let globalAccessToken;
-let globalRefreshToken;
 /** **************OAUTH**************** */
 
-// var FitbitStrategy = require('passport-fitbit-oauth2').FitbitOAuth2Strategy;;
-
-passport.use(new FitbitStrategy({
-  clientID: config.fitbit.id,
-  clientSecret: config.fitbit.secret,
-  scope: ['activity', 'profile', 'sleep', 'social'],
-  callbackURL: 'http://localhost:8080/callback',
-},
-  ((accessToken, refreshToken, profile, done) => {
-  // User.findOrCreate({
-  //   fitbitId: profile.id
-  // }, function (err, user) {
-  //   return done(err, user);
-  // });
+passport.use(new FitbitStrategy(
+  {
+    clientID: config.fitbit.id,
+    clientSecret: config.fitbit.secret,
+    scope: ['activity', 'profile', 'sleep', 'social'],
+    callbackURL: 'http://localhost:8080/callback',
+  },
+  (accessToken, refreshToken, profile, done) => {
+    // User.findOrCreate({
+    //   fitbitId: profile.id
+    // }, function (err, user) {
+    //   return done(err, user);
+    // });
     globalAccessToken = accessToken; // should go in db
     globalRefreshToken = refreshToken; // should go in db
     globalProfile = profile;
     return done(null, profile);
-  }),
+  },
 ));
 
 passport.serializeUser((user, done) => {
-  console.log('serializing');
   done(null, user);
 });
 
 passport.deserializeUser((user, done) => {
-  console.log('deserializing');
   done(null, user);
 });
 
 app.get(
   '/auth/fitbit',
-  passport.authenticate('fitbit', {
-    scope: ['activity', 'heartrate', 'location', 'profile'],
-  }),
+  passport.authenticate(
+    'fitbit',
+    {
+      scope: ['activity', 'heartrate', 'location', 'profile'],
+    },
+  ),
 );
 
 app.get('/callback', passport.authenticate('fitbit', {
@@ -80,20 +76,16 @@ app.get('/callback', passport.authenticate('fitbit', {
 }));
 
 app.get('/auth/fitbit/success', (req, res) => {
-  console.log(req.session.passport.user._json);
-  console.log('hooray!');
   res.redirect('/');
 });
 
 app.get('/auth/fitbit/failure', (req, res) => {
-  console.log('boo didn\'t work!');
-  res.json({ err: 'failure!' });
+  res.status(401).json({ err: 'failure!' });
 });
 
 /** *********************************************** */
 
 app.get('/user', (req, res) => {
-  console.log(req.session);
   if (req.session.passport) {
     res.json({ user: req.session.passport.user.displayName });
   } else {
@@ -112,13 +104,12 @@ app.get('/fitbit/lifetime', (req, res) => {
       res.json(data.data); // TODO: replace this with db storage?
     })
     .catch((err) => {
-      console.log('eroror', err);
-      res.send(err);
+      res.status(401).send(err);
     });
 });
 
 app.get('/fitbit/dailySummary', (req, res) => {
-  const date = req.query.date; // must be in YYYY-MM-DD format string
+  const { date } = req.query; // must be in YYYY-MM-DD format string
   axios.get(`https://api.fitbit.com/1/user/-/activities/date/${date}.json`, {
     headers: {
       Authorization: `Bearer ${globalAccessToken}`, // TODO: replace this with db call based on req.session.passport.user.id
