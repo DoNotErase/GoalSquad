2242e566f9fc3bde8cf4b2b975038177442b08bc
Marci McClenon <mccmarci@gmail.com>
2018-03-14 19:21:19 -0500

    fitbit auth route runs, no storage

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,11 +1,20 @@
 var mysql = require('mysql');
+var config = require('../config');
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'test'
+  host: config.aws.RDS_HOSTNAME,
+  user: config.aws.RDS_USERNAME,
+  password: config.aws.RDS_PASSWORD,
+  port: config.aws.RDS_PORT,
+  database : 'goalsquad'
 });
+
+// var connection = mysql.createConnection({
+//   host: process.env.RDS_HOSTNAME,
+//   user: process.env.RDS_USERNAME,
+//   password: process.env.RDS_PASSWORD,
+//   port: process.env.RDS_PORT
+// });
 
 var selectAll = function(callback) {
   connection.query('SELECT * FROM items', function(err, results, fields) {
--- server/index.js
+++ server/index.js
@@ -1,23 +1,80 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var items = require('../database-mysql');
-
+var session = require('express-session');
+var FitbitStrategy = require('passport-fitbit-oauth2').FitbitOAuth2Strategy;
+var passport = require('passport');
+var config = require('../config.js');
 
 var app = express();
+
+var db = require('../database-mysql');
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.use(session({
+  secret: 'keyboard cat'
+}));
+
+app.use(passport.initialize());
+app.use(passport.session({
+  resave: false,
+  saveUninitialized: true
+}));
+
+let logger = function (req, res, next) {
+  console.log('!!!!', req.url);
+  next();
+}
+app.use(logger);
+
+var FitbitStrategy = require('passport-fitbit-oauth2').FitbitOAuth2Strategy;;
+
+passport.use(new FitbitStrategy({
+    clientID: config.fitbit.id,
+    clientSecret: config.fitbit.secret,
+    scope: ['activity', 'profile', 'sleep', 'social'],
+    callbackURL: "http://127.0.0.1:3000/auth/fitbit/callback"
+  },
+  function (accessToken, refreshToken, profile, done) {
+    // User.findOrCreate({
+    //   fitbitId: profile.id
+    // }, function (err, user) {
+    //   return done(err, user);
+    // });
+    console.log('--------------profile---------------', profile);
+    return done(null, profile)
+  }
+));
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
 });
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+app.get('/auth/fitbit',
+  passport.authenticate('fitbit', {
+    scope: ['activity', 'heartrate', 'location', 'profile']
+  }));
+
+app.get('/auth/fitbit/callback', passport.authenticate('fitbit', {
+  successRedirect: '/auth/fitbit/success',
+  failureRedirect: '/auth/fitbit/failure'
+}));
+
+app.get('/auth/fitbit/success', function (req, res) {
+  console.log(req.body, req.headers);
+  console.log('hooray!');
+  res.redirect('/');
+})
+
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
