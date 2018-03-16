cf3ba294a5f2cd6e25c187c8393c60c37467de8d
Jacob Granberry <jacobgranberry@gmail.com>
2018-03-16 09:50:48 -0500

    mwrge and linting

--- react-client/src/components/HomePage.jsx
+++ react-client/src/components/HomePage.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import { Header, Grid, Button, Segment, Container } from 'semantic-ui-react';
+import { Header, Grid } from 'semantic-ui-react';
 
-const HomePage = (props) => {
-    return (
-        <div className='homepage'>
-        <style>{`
-            body > div,
-            body > div > div,
-            body > div > div > div.homepage {
-                height: 100%;
-            }
-        `}</style>
-            <Grid 
-              textAlign='center'
-              verticalAlign='middle'
-              style={{ height: '80%' }} >
-                <Grid.Row>
-                    <Grid.Column width={10} color='blue'>
-                        
-                            <Header as='h1' textAlign='center' content='GOAL'/>
-                            <Header as='h1' textAlign='center' content='SQUAD'/>
-                        
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-       </div>
-    ) 
-}
+const HomePage = () => (
+  <div className="homepage">
+    <style>{`
+        body > div,
+        body > div > div,
+        body > div > div > div.homepage {
+            height: 100%;
+        }
+    `}
+    </style>
+    <Grid
+      textAlign="center"
+      verticalAlign="middle"
+      style={{ height: '80%' }}
+    >
+      <Grid.Row>
+        <Grid.Column width={10} color="blue">
+          <Header as="h1" textAlign="center" content="GOAL" />
+          <Header as="h1" textAlign="center" content="SQUAD" />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </div>
+);
 
 export default HomePage;
