eb3e8961c92bac7b95921a9ac817c00abda13944
Jacob Granberry <jacobgranberry@gmail.com>
2018-03-16 15:40:45 -0500

    fixes linting on root

--- react-client/src/components/BarnPage.jsx
+++ react-client/src/components/BarnPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Header, Image } from 'semantic-ui-react';
+import { Header, Image, Link } from 'semantic-ui-react';
 
 const BarnPage = () => (
   <div className="barnpage">
     <Header as="h2">Hi</Header>
-    <Image src="./assets/icons/eggTEST.png" />
+    <Image
+      src="./assets/icons/eggTEST.png"
+      as={Link}
+      to="/goals"
+      floated="right"
+    />
   </div>
 );
 
