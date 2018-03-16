33dc0aa2d8df879accbfc308e496298f20c7be59
Jacob Granberry <jacobgranberry@gmail.com>
2018-03-16 16:44:37 -0500

    fix linter

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Root from './components/Root.jsx';
-import store from './store/index.js';
+import Root from './components/Root';
+import store from './store/index';
 
 render(
   <Root store={store} />,
