e53190230decd12c6ce1b8515c737886a53e2845
Sean Quillen <sean.r.quillen@gmail.com>
2018-03-16 12:16:18 -0500

    adds semantic ui formatting

--- react-client/src/components/GoalsPage.jsx
+++ react-client/src/components/GoalsPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 
 class GoalsPage extends React.Component {
 	constructor(props) {
@@ -7,8 +8,27 @@ class GoalsPage extends React.Component {
 
 	render() {
 		return (
-			<div>
-			hi
+			<div className="goals-page">
+			<style>{`
+			    body > div,
+			    body > div > div,
+			    body > div > div > div.goals-page {
+			        height: 100%;
+			    }
+			`}
+			</style>
+				<Grid
+				  textAlign="center"
+				  verticalAlign="middle"
+				  style={{ height: '100%' }}
+				>
+				<Grid.Column width={10} style={{ maxWidth: 450 }}>
+	        <Header as="h1" textAlign="left" className="white" content="Your Goals" />
+	        <a href="/auth/fitbit">
+	          <Button color="violet" fluid size="large" style={{ marginTop: 250 }}>Connect</Button>
+	        </a>
+				</Grid.Column>
+				</Grid>
 			</div>
 		)
 	}
