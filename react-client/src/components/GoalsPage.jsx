8fc90240b97f8b31326907a374143e154399a1a5
Sean Quillen <sean.r.quillen@gmail.com>
2018-03-16 12:05:29 -0500

    adds generic router to server and renders goals page

---
+++ react-client/src/components/GoalsPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class GoalsPage extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div>
+			hi
+			</div>
+		)
+	}
+}
+
+export default GoalsPage;
