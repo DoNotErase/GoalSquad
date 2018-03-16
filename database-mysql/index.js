028157e18fbea8f32b0bebad42f9cc7e52df7835
ShahzebMeghani <shahzebmeghani@gmail.com>
2018-03-15 19:55:25 -0500

    created test db query and test endpoint. Fixed depecration warnings

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,17 +1,30 @@
 const mysql = require('mysql');
 const config = require('../config');
 const Promise = require('bluebird');
-//needed for mysql
-Promise.promisifyAll(require("mysql/lib/Connection").prototype);
-Promise.promisifyAll(require("mysql/lib/Pool").prototype);
+// needed for mysql
+Promise.promisifyAll(require('mysql/lib/Connection').prototype);
+Promise.promisifyAll(require('mysql/lib/Pool').prototype);
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host: config.aws.RDS_HOSTNAME,
   user: config.aws.RDS_USERNAME,
   password: config.aws.RDS_PASSWORD,
   port: config.aws.RDS_PORT,
-  database : 'goalsquad'
+  database: 'goalsquad',
 });
 
+// test function for example
+const findUserId = async () => {
+  try {
+    const result = await db.queryAsync('SELECT * FROM user');
+    console.log(result);
+    return result;
+  } catch (e) {
+    return e;
+  }
+};
 
-module.exports.selectAll = selectAll;
+module.exports = {
+  db,
+  findUserId,
+};
