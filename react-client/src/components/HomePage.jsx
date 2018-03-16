7997a859d18f5ec44b5bd5e06770ace703605d93
Marci McClenon <mccmarci@gmail.com>
2018-03-16 11:51:10 -0500

    updates reducer for authentication

--- react-client/src/components/HomePage.jsx
+++ react-client/src/components/HomePage.jsx
@@ -4,30 +4,50 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actions from '../actions/homePageActions';
 
-const HomePage = () => (
-  <div className="homepage">
-    <style>{`
-        body > div,
-        body > div > div,
-        body > div > div > div.homepage {
-            height: 100%;
-        }
-    `}
-    </style>
-    <Grid
-      textAlign="center"
-      verticalAlign="middle"
-      style={{ height: '100%' }}
-    >
-      <Grid.Column width={10} style={{ maxWidth: 450 }}>
-        <Header as="h1" textAlign="center" className="white" content="GOAL" />
-        <Header as="h1" textAlign="center" className="white" content="SQUAD" />
-        <a href="/auth/fitbit">
-          <Button color="violet" fluid size="large" style={{ marginTop: 250 }}>Connect</Button>
-        </a>
-      </Grid.Column>
-    </Grid>
-  </div>
-);
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  componentWillMount() {
+    this.props.actions.attemptLogin();
+  }
 
-export default HomePage;
+  componentDidMount() {
+    if (this.state.authenticated) {
+      this.history.push('/landingPage');
+    }
+  }
+  render() {
+    return (
+      <div className="homepage">
+        <style>{`
+            body > div,
+            body > div > div,
+            body > div > div > div.homepage {
+                height: 100%;
+            }
+        `}
+        </style>
+        <Grid
+          textAlign="center"
+          verticalAlign="middle"
+          style={{ height: '100%' }}
+        >
+          <Grid.Column width={10} style={{ maxWidth: 450 }}>
+            <Header as="h1" textAlign="center" className="white" content="GOAL" />
+            <Header as="h1" textAlign="center" className="white" content="SQUAD" />
+            <a href="/auth/fitbit">
+              <Button color="violet" fluid size="large" style={{ marginTop: 250 }}>Connect</Button>
+            </a>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({ actions: bindActionCreators(actions, dispatch) });
+const mapStateToProps = state => ({ ...state });
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
--- react-client/src/reducers/mainReducer.js
+++ react-client/src/reducers/mainReducer.js
@@ -1,21 +1,27 @@
-
 const mainReducer = (state = {}, action) => {
-    switch(action.type) {
-        case 'TEST': {
-            return {
-                ...state
-            }
-        }
-        case 'USER_LIFETIME_ACTIVITY': {
-            return {
-                ...state, 
-                lifetimeSteps : action.payload.lifetimeSteps,
-                lifetimeFloors: action.payload.lifetimeFloors,
-                lifetimeDistance: action.payload.lifetimeDistance,
-            }
-        }
-        default: return state;
+  switch (action.type) {
+    case 'TEST': {
+      return {
+        ...state,
+      };
     }
-}
+    case 'USER_LIFETIME_ACTIVITY': {
+      return {
+        ...state,
+        lifetimeSteps: action.payload.lifetimeSteps,
+        lifetimeFloors: action.payload.lifetimeFloors,
+        lifetimeDistance: action.payload.lifetimeDistance,
+      };
+    }
+    case 'USER_LOGIN': {
+      return {
+        ...state,
+        userID: action.payload.id,
+        username: action.username.username,
+      };
+    }
+    default: return state;
+  }
+};
 
 export default mainReducer;
