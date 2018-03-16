51b1d8da56ba8155dce923d530da7cfcb345324f
Sean Quillen <sean.r.quillen@gmail.com>
2018-03-16 17:14:39 -0500

    fixes formatting

--- react-client/src/components/IncubatorPage.jsx
+++ react-client/src/components/IncubatorPage.jsx
@@ -16,8 +16,9 @@ class IncubatorPage extends React.Component {
 			goalContainer: {
 				border: '1px solid black',
 				borderRadius: '5px',
-				minHeight: '500px',
-				paddingTop: '30px'
+				// minHeight: '500px',
+				// paddingTop: '30px'
+				minHeight: '100vh'
 			},
 			goals: {
 				marginLeft: '25px',
@@ -27,20 +28,13 @@ class IncubatorPage extends React.Component {
 
 		return (
 			<div className="goals-page">
-			<style>{`
-			    body > div,
-			    body > div > div,
-			    body > div > div > div.goals-page {
-			        height: 100%;
-			    }
-			`}
-			</style>
+			
 				<Grid
 				  textAlign="center"
 				  verticalAlign="middle"
 				  style={{ height: '100%' }}
 				>
-				<Grid.Column width={16} className="goal-page" style={{ maxWidth: 500 }}>
+				<Grid.Column width={16} className="goal-page">
 					<div style={ styles.goalContainer }>
 		        <Header size="large" textAlign="left">Your Goals</Header>
 		        <Divider fitted />
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const FitbitStrategy = require('passport-fitbit-oauth2').FitbitOAuth2Strategy;
 const passport = require('passport');
 const config = require('../config.js');
 const axios = require('axios');
+const path = require('path')
 
 const app = express();
 const db = require('../database-mysql/index.js');
@@ -133,6 +134,10 @@ app.get('/fitbit/dailySummary', async (req, res) => {
 app.get('/test', async (req, res) => {
   res.json(await db.findUserId());
 });
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../react-client/dist', '/index.html'));
+})
 
 app.listen(8080, () => {
   console.log('listening on port 8080!');
