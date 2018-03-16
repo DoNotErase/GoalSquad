c15007560c081021cda5afd1aca7153ded1bfac0
Jacob Granberry <jacobgranberry@gmail.com>
2018-03-16 16:13:45 -0500

    barn style tweaks

--- react-client/src/components/BarnPage.jsx
+++ react-client/src/components/BarnPage.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import { Header, Image, Link } from 'semantic-ui-react';
-
-const styles = {
-  eggStyle: {
-    bottom: 0,
-    right: 0,
-  },
-};
+import { Header, Image, Link, Container } from 'semantic-ui-react';
 
 const BarnPage = () => (
   <div className="barnpage">
     <Header as="h2">Hi</Header>
-    <Image
-      style={styles.eggStyle}
-      src="./assets/icons/eggTEST.png"
-      as={Link}
-      href="/"
-      floated="right"
-    />
+    <Container>
+      <Image
+        src="./assets/icons/eggTEST.png"
+        as={Link}
+        href="/"
+        verticalAlign="bottom"
+        floated="right"
+      />
+    </Container>
   </div>
 );
 
