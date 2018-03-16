f5334a235d02911adcaeb1b0d4eddb9b7311a968
Jacob Granberry <jacobgranberry@gmail.com>
2018-03-16 16:05:13 -0500

    fixes egg route

--- react-client/src/components/BarnPage.jsx
+++ react-client/src/components/BarnPage.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Header, Image, Link } from 'semantic-ui-react';
 
+const styles = {
+  eggStyle: {
+    bottom: 0,
+    right: 0,
+  },
+};
+
 const BarnPage = () => (
   <div className="barnpage">
     <Header as="h2">Hi</Header>
     <Image
+      style={styles.eggStyle}
       src="./assets/icons/eggTEST.png"
       as={Link}
-      to="/goals"
+      href="/"
       floated="right"
     />
   </div>
 );
+
 
 export default BarnPage;
