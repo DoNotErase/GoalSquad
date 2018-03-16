ca8cebb3978ea2de1c787923f8d9492824af7d8c
Jacob Granberry <jacobgranberry@gmail.com>
2018-03-16 15:04:16 -0500

    adds testegg image

--- react-client/src/components/BarnPage.jsx
+++ react-client/src/components/BarnPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Image } from 'semantic-ui-react';
 
 const BarnPage = () => (
   <div className="barnpage">
     <Header as="h2">Hi</Header>
+    <Image src="/dist/assests/icons.eggTEST.png" />
   </div>
 );
 
