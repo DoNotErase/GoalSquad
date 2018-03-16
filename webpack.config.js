1410044184e0b851685b080b5de3add5703dd6d0
Jacob Granberry <jacobgranberry@gmail.com>
2018-03-16 14:56:34 -0500

    eslints webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/react-client/src');
-var DIST_DIR = path.join(__dirname, '/react-client/dist');
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/react-client/src');
+const DIST_DIR = path.join(__dirname, '/react-client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
   },
-  module : {
-    loaders : [
+  resolve: { extensions: ['.js', '.jsx', '*'] },
+  module: {
+    loaders: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
-       }
-      }
-    ]
+          presets: ['react', 'es2015'],
+        },
+      },
+    ],
   },
   devServer: {
-    historyApiFallback: true  // serves up main page for react-router to work with redux
-  }
+    historyApiFallback: true, // serves up main page for react-router to work with redux
+  },
 };
