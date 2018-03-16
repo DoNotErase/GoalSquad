9332d52fb431733774e1f7735798e853999c292b
Sean Quillen <sean.r.quillen@gmail.com>
2018-03-16 09:48:27 -0500

    adds firebase, service workers, and style page

---
+++ react-client/dist/sw.js
@@ -0,0 +1,29 @@
+var cacheName = 'goalsquad-page';
+
+var filesToCache = [
+  './',
+	'./index.html',
+	'./index.jsx'
+];
+
+self.addEventListener('install', event => {
+	console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
