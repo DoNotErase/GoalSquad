fc553dc87ee310b921d03d784f8ce174a39d2c0e
Jacob Granberry <jacobgranberry@gmail.com>
2018-03-17 11:08:46 -0500

    adds Goal component and Goal page

--- react-client/src/components/GoalsPage.jsx
+++ react-client/src/components/GoalsPage.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Accordion, Icon, Segment } from 'semantic-ui-react';
+import Goal from './Goal';
 
 class GoalsPage extends React.Component {
   constructor() {
     super();
     this.state = {
-      activeIndex: 0,
+      activeIndex: -1,
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(e, titleProps) {
     const { index } = titleProps;
-    const { activeIndex } = this.state.activeIndex;
+    const { activeIndex } = this.state;
     const newIndex = activeIndex === index ? -1 : index;
 
     this.setState({ activeIndex: newIndex });
@@ -28,15 +29,7 @@ class GoalsPage extends React.Component {
         </Accordion.Title>
         <Accordion.Content active={activeIndex === 0}>
           <Segment.Group raised>
-            <Segment padded color="green">
-            Goal 1
-            </Segment>
-            <Segment padded color="yellow">
-            Goal 2
-            </Segment>
-            <Segment padded color="red">
-            Goal 3
-            </Segment>
+            <Goal />
           </Segment.Group>
         </Accordion.Content>
 
@@ -45,17 +38,9 @@ class GoalsPage extends React.Component {
            Steps
         </Accordion.Title>
         <Accordion.Content active={activeIndex === 1}>
-            <Segment.Group raised>
-                <Segment padded color="green">
-                Goal 1
-                </Segment>
-                <Segment padded color="yellow">
-                Goal 2
-                </Segment>
-                <Segment padded color="red">
-                Goal 3
-                </Segment>
-            </Segment.Group>
+          <Segment.Group raised>
+            <Goal />
+          </Segment.Group>
         </Accordion.Content>
 
         <Accordion.Title active={activeIndex === 2} index={2} onClick={this.handleClick}>
@@ -63,17 +48,9 @@ class GoalsPage extends React.Component {
            Stairs
         </Accordion.Title>
         <Accordion.Content active={activeIndex === 2}>
-            <Segment.Group raised>
-                <Segment padded color="green">
-                Goal 1
-                </Segment>
-                <Segment padded color="yellow">
-                Goal 2
-                </Segment>
-                <Segment padded color="red">
-                Goal 3
-                </Segment>
-            </Segment.Group>
+          <Segment.Group raised>
+            <Goal />
+          </Segment.Group>
         </Accordion.Content>
       </Accordion>
     );
