96f7dd9b78ef34bfcf15220e58750cd38cf46ac5
Marci McClenon <mccmarci@gmail.com>
2018-03-20 16:51:29 -0500

    more linting

--- react-client/src/components/BarnPage.jsx
+++ react-client/src/components/BarnPage.jsx
@@ -3,16 +3,15 @@ import { Grid } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
+import HorizontalScroll from 'react-scroll-horizontal';
 import * as actions from '../actions/actions';
 import * as barnActions from '../actions/barnActions';
 import MainMenu from './MainMenu';
-import HorizontalScroll from 'react-scroll-horizontal';
 
-class BarnPage extends React.Component {
-  render() {
-    const child = { width: '700px', height: '100%', backgroundColor: 'white' };
-    return (
-      <div className="barnpage">
+const BarnPage = () => {
+  const child = { width: '700px', height: '100%', backgroundColor: 'white' };
+  return (
+    <div className="barnpage">
       <HorizontalScroll>
         <Grid style={child}>
           <Grid.Column
@@ -23,22 +22,20 @@ class BarnPage extends React.Component {
             verticalAlign="bottom"
           />
         </Grid>
-        </HorizontalScroll>
-        <MainMenu />
-      </div>
-
-    );
-  }
-}
+      </HorizontalScroll>
+      <MainMenu />
+    </div>
+  );
+};
 
 BarnPage.propTypes = {
-  state: PropTypes.shape({
-    id: PropTypes.string,
-    username: PropTypes.string,
-  }).isRequired,
-  actions: PropTypes.objectOf(PropTypes.func).isRequired,
-  barnState: PropTypes.objectOf(PropTypes.string).isRequired,
-  barnActions: PropTypes.objectOf(PropTypes.func).isRequired,
+  // state: PropTypes.shape({
+  //   id: PropTypes.string,
+  //   username: PropTypes.string,
+  // }).isRequired,
+  // actions: PropTypes.objectOf(PropTypes.func).isRequired,
+  // barnState: PropTypes.objectOf(PropTypes.string).isRequired,
+  // barnActions: PropTypes.objectOf(PropTypes.func).isRequired,
 };
 
 const mapDispatchToProps = dispatch => (
--- react-client/src/components/IncubatorPage.jsx
+++ react-client/src/components/IncubatorPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Header, Divider, Segment } from 'semantic-ui-react';
+import { Grid, Header, Divider } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
