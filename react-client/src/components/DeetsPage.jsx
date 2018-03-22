484dac899b30148afa371588be022d7db5e89c67
Jacob Granberry <jacobgranberry@gmail.com>
2018-03-22 13:57:40 -0500

    stubs out deets page for now

--- react-client/src/components/DeetsPage.jsx
+++ react-client/src/components/DeetsPage.jsx
@@ -10,8 +10,8 @@ const DeetsPage = () => (
     <Divider hidden />
     <Grid centered>
       <Grid.Column computer={8} mobile={16}>
-        <Segment.Group>
-          <Segment>
+        <Segment.Group raised>
+          <Segment compact>
             <Grid centered>
               <Grid.Row columns={1}>
                 <Grid.Column>
@@ -31,6 +31,29 @@ const DeetsPage = () => (
               </Grid.Row>
             </Grid>
           </Segment>
+          <Segment attached="bottom" compact>
+            <Grid centered>
+              <Grid.Row columns={1}>
+                <Grid.Column>
+                  <Statistic.Group size="mini">
+                    <Statistic horizontal>
+                      <Statistic.Value>10%</Statistic.Value>
+                      <Statistic.Label>Global<br />Average</Statistic.Label>
+                    </Statistic>
+                    <Statistic horizontal>
+                      <Statistic.Value>30%</Statistic.Value>
+                      <Statistic.Label>Global<br />Average</Statistic.Label>
+                    </Statistic>
+                  </Statistic.Group>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Segment>
+        </Segment.Group>
+        <Segment.Group raised>
+            <Segment><Statistic horizontal size="tiny" label="Total Stairs" value="2,204" /></Segment>
+            <Segment><Statistic horizontal size="tiny" label="Total Steps" value="100,412" /></Segment>
+            <Segment><Statistic horizontal size="tiny" label="Total Miles" value="157" /></Segment>
         </Segment.Group>
       </Grid.Column>
       <MainMenu />
