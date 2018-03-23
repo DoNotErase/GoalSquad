2172ca4699687626a45a7157aa0a929bcc9b414d
Jacob Granberry <jacobgranberry@gmail.com>
2018-03-22 19:26:25 -0500

    makes squaddie card modal work

--- react-client/src/components/SquaddieCard.jsx
+++ react-client/src/components/SquaddieCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Modal } from 'semantic-ui-react';
+import { Card, Modal, Icon, Image } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
 class SquaddieCard extends React.Component {
@@ -12,23 +12,52 @@ class SquaddieCard extends React.Component {
     this.close = this.close.bind(this);
   }
 
-  show(dimmer) { this.setState({ dimmer, open: true }); }
+  show(dimmer, size) { this.setState({ dimmer, size, open: true }); }
   close() { this.setState({ open: false }); }
 
   render() {
     const src = './assets/misc/testcardimage.png';
-    const { open, dimmer } = this.state;
+    const { open, dimmer, size } = this.state;
 
     return (
 
       <Modal
-        trigger={<Card raised image={src} description="Squaddie" onClick={() => this.show('blurring')} />}
+        trigger={
+          <Card
+            raised
+            image={src}
+            description="Squaddie"
+            onClick={() => this.show('blurring', 'tiny')}
+          />
+        }
+        size={size}
         dimmer={dimmer}
         open={open}
         onClose={this.close}
       >
         <Modal.Content>
-            test
+          <Card centered fluid>
+            <Image src="./assets/squaddies/squaddie-2.png" />
+            <Card.Content>
+              <Card.Header>
+        Matthew
+              </Card.Header>
+              <Card.Meta>
+                <span className="date">
+          Joined in 2015
+              </span>
+              </Card.Meta>
+              <Card.Description>
+        Matthew is a musician living in Nashville.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name="user" />
+        22 Friends
+              </a>
+            </Card.Content>
+          </Card>
         </Modal.Content>
       </Modal>
 
