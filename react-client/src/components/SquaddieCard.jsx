ba7c0f9f3e88bc8ad345b57e1829116966d5a68f
Jacob Granberry <jacobgranberry@gmail.com>
2018-03-23 17:04:09 -0500

    adjusts card content

--- react-client/src/components/SquaddieCard.jsx
+++ react-client/src/components/SquaddieCard.jsx
@@ -42,20 +42,9 @@ class SquaddieCard extends React.Component {
               <Card.Header>
                 {this.props.squaddie.monster_name}
               </Card.Header>
-              <Card.Meta>
-                <span className="date">
-                  Joined in 2015
-                </span>
-              </Card.Meta>
               <Card.Description>
                 {this.props.squaddie.monster_description}
               </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a>
-                <Icon name="user" />
-                22 Friends
-              </a>
             </Card.Content>
           </Card>
         </Modal.Content>
