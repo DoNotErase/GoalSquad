0607dca7290d3752163982360cae0360a989b61b
Jacob Granberry <jacobgranberry@gmail.com>
2018-03-27 17:25:39 -0500

    removes redux tools

--- react-client/src/store/index.js
+++ react-client/src/store/index.js
@@ -1,6 +1,6 @@
 import thunk from 'redux-thunk';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+// import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 import { browserHistory } from 'react-router';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
 import mainReducer from '../reducers/mainReducer';
@@ -22,7 +22,7 @@ const reducer = combineReducers({ // combines reducers from reducer folder
 // creates store and allows for chrome redux plugin
 const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(thunk, routerMiddleware(browserHistory))),
+  applyMiddleware(thunk, routerMiddleware(browserHistory)),
 );
 
 export default store;
