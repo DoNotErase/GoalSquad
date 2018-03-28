ee87aacf3ddb6ed7085225d75d17853cc17c5dfd
Jacob Granberry <jacobgranberry@gmail.com>
2018-03-28 12:13:43 -0500

    adds new favicon and battle pages

---
+++ react-client/src/components/BattleInterface.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class BattleInterface extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default connect(null, null)(BattleInterface);
---
+++ react-client/src/components/BattlePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import BattleInterface from './BattleInterface';
+
+class BattlePage extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default connect(null, null)(BattlePage);
