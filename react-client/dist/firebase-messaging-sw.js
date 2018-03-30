ff2d7826cd339efe2a3386167782d7537e0cd6f3
Sean Quillen <sean.r.quillen@gmail.com>
2018-03-30 11:03:39 -0500

    fixes messange sw call

--- react-client/dist/firebase-messaging-sw.js
+++ react-client/dist/firebase-messaging-sw.js
@@ -1,18 +1,14 @@
 importScripts('https://www.gstatic.com/firebasejs/4.12.1/firebase.js');
 importScripts('https://www.gstatic.com/firebasejs/4.12.1/firebase-messaging.js');
 
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDbOhdxctAyUdSrr0xbDxSazzQfu1wYeNY",
-    authDomain: "goalsquad-f12a7.firebaseapp.com",
-    databaseURL: "https://goalsquad-f12a7.firebaseio.com",
-    projectId: "goalsquad-f12a7",
-    storageBucket: "goalsquad-f12a7.appspot.com",
-    messagingSenderId: "177647825623"
-  };
-  firebase.initializeApp(config);
-</script>
+var config = {
+  apiKey: "AIzaSyDbOhdxctAyUdSrr0xbDxSazzQfu1wYeNY",
+  authDomain: "goalsquad-f12a7.firebaseapp.com",
+  databaseURL: "https://goalsquad-f12a7.firebaseio.com",
+  projectId: "goalsquad-f12a7",
+  storageBucket: "goalsquad-f12a7.appspot.com",
+  messagingSenderId: "177647825623"
+};
 
 firebase.initializeApp(config);
 const messaging = firebase.messaging();
