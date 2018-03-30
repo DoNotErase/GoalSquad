4427e6c26869156dadd0b14d4ee61bdff0804745
Sean Quillen <sean.r.quillen@gmail.com>
2018-03-30 10:51:51 -0500

    updates messenger sw script

--- react-client/dist/firebase-messaging-sw.js
+++ react-client/dist/firebase-messaging-sw.js
@@ -1,14 +1,18 @@
-importScripts('https://www.gstatic.com/firebasejs/4.11.0/firebase.js');
-importScripts('https://www.gstatic.com/firebasejs/4.11.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/4.12.1/firebase.js');
+importScripts('https://www.gstatic.com/firebasejs/4.12.1/firebase-messaging.js');
 
-var config = {
-  apiKey: "AIzaSyDbOhdxctAyUdSrr0xbDxSazzQfu1wYeNY",
-  authDomain: "https://goalsquad-f12a7.firebaseapp.com",
-  databaseURL: "https://goalsquad-f12a7.firebaseio.com",
-  projectId: "goalsquad-f12a7",
-  // storageBucket: "",
-  messagingSenderId: "177647825623"
-};
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDbOhdxctAyUdSrr0xbDxSazzQfu1wYeNY",
+    authDomain: "goalsquad-f12a7.firebaseapp.com",
+    databaseURL: "https://goalsquad-f12a7.firebaseio.com",
+    projectId: "goalsquad-f12a7",
+    storageBucket: "goalsquad-f12a7.appspot.com",
+    messagingSenderId: "177647825623"
+  };
+  firebase.initializeApp(config);
+</script>
 
 firebase.initializeApp(config);
 const messaging = firebase.messaging();
