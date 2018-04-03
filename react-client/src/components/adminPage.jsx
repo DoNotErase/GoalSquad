6a816f47ba2cea8817d6376b74483fc610c4e499
Sean Quillen <sean.r.quillen@gmail.com>
2018-04-02 19:56:05 -0500

    adds in basic admin page to send notifications from

---
+++ react-client/src/components/adminPage.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+
+
+class adminPage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			notification: ''
+		}
+	}
+
+	updateNotification(e) { this.setState({ notification: e.target.value }); }
+
+	sendNotification(e) {
+  		e.preventDefault();
+  		let notification  = this.props.notification;
+  		if ( !notificationMessage ) return;
+
+  		FIREBASE_DATABASE.ref('/notifications').push({
+      		user: 'ADMIN',
+      		message: notificationMessage,
+    	})
+    		.then(() => {
+      		this.setState({ notification: '' });
+    		})
+    		.catch(() => {
+      		console.log("Unable to send notification to Firebase database")
+    		});
+}
+
+	render() {
+		return (
+			<div>
+				<input value={this.state.notification} onChange={this.updateNotification} type="text"/>
+				<input type="submit" onClick={this.sendNotification(e)}/>
+			</div>
+		)
+	}
+
+
+}
+
+
