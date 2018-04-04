446ac97472fe61d0fa05597eb5b43535d6670173
Jacob Granberry <jacobgranberry@gmail.com>
2018-04-04 10:42:14 -0500

    refactor sw

--- react-client/dist/sw.js
+++ react-client/dist/sw.js
@@ -1,13 +1,13 @@
-var cacheName = 'goalsquad-page';
+const cacheName = 'goalsquad-page';
 
-var filesToCache = ['/', '/index.html', '/styles.css'];
+const filesToCache = ['/', '/index.html', '/styles.css'];
 
 self.addEventListener('install', (event) => {
   console.log('[ServiceWorker] Install');
   event.waitUntil(caches.open(cacheName).then((cache) => {
     console.log('[ServiceWorker] Caching app shell');
     return cache.addAll(filesToCache);
-  }) );
+  }));
 });
 
 self.addEventListener('activate', (event) => {
@@ -15,22 +15,20 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-	event.respondWith(caches.open(filesToCache)
-		.then(function(cache) {
-	  	return cache.match(event.request)
-	  .then(function(response) {
-	    var fetchPromise = fetch(event.request) // used at end of request (if fetched)
-	  .then(function(networkResponse) {
-	    // if a response was received, update the cache
-      if (networkResponse) {
-        cache.put(event.request, networkResponse.clone());
-      }
-        return networkResponse;
-      }, function (e) {
-      	 // if the event of an error, do nothing since we're offline
-      });
+  event.respondWith(caches.open(filesToCache)
+    .then(cache => cache.match(event.request)
+      .then((response) => {
+        const fetchPromise = fetch(event.request) // used at end of request (if fetched)
+          .then((networkResponse) => {
+            // if a response was received, update the cache
+            if (networkResponse) {
+              cache.put(event.request, networkResponse.clone());
+            }
+            return networkResponse;
+          }, (e) => {
+            // if the event of an error, do nothing since we're offline
+          });
         // respond from the cache, or the network
         return response || fetchPromise;
-	    });
-	}));
+      })));
 });
