94a1de2ce9728df9b8260ff5e303b4960a20da97
Marci McClenon <mccmarci@gmail.com>
2018-05-29 15:29:05 -0500

    finished paramerterizing

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -221,6 +221,12 @@ module.exports.createCustomGoal = async (goalObj) => {
         db.queryAsync(attachUserWithLength, [userID, goalID, goalObj.goalAmount, goalObj.points, goalObj.goalLength.days, goalObj.goalLength.hours]),
         db.queryAsync(updateUserCustomTimers, [userID]),
       ]);
+
+      const setEndDate = 'UPDATE user_goal SET user_goal_end_date = ' +
+        '(SELECT DATE_ADD((SELECT DATE_ADD((SELECT MAX(user_goal_start_date)), ' +
+        'INTERVAL ? DAY)), INTERVAL ? HOUR)) WHERE user_goal_id = ?';
+
+      await db.queryAsync(setEndDate, [goalObj.goalLength.days, goalObj.goalLength.hours, goalID]);
     } else {
       attachUserNoLength = 'INSERT INTO user_goal (user_id, goal_id, user_goal_start_value, user_goal_current, ' +
         'user_goal_target, user_goal_points, user_goal_start_date) VALUES ' +
@@ -231,18 +237,6 @@ module.exports.createCustomGoal = async (goalObj) => {
         db.queryAsync(updateUserCustomTimers, [userID]),
       ]);
     }
-
-    if (goalObj.goalLength) {
-      const setEndDate = 'UPDATE user_goal SET user_goal_end_date = ' +
-        '(SELECT DATE_ADD((SELECT DATE_ADD((SELECT MAX(user_goal_start_date)), ' +
-        `INTERVAL ${goalObj.goalLength.days} DAY)), ` +
-        `INTERVAL ${goalObj.goalLength.hours} HOUR)) ` +
-        `WHERE user_goal_id = (${goalID});`;
-
-      await db.queryAsync(setEndDate);
-    }
-
-    return '';
   } catch (err) {
     console.log(err);
     throw new Error('trouble in createUserGoal');
@@ -251,9 +245,9 @@ module.exports.createCustomGoal = async (goalObj) => {
 
 module.exports.getDefaultGoals = async () => {
   try {
-    const query = 'SELECT * FROM goal WHERE goal_class != "custom"';
+    const fetchGoals = 'SELECT * FROM goal WHERE goal_class != "custom"';
 
-    const allDefaultGoals = await db.queryAsync(query);
+    const allDefaultGoals = await db.queryAsync(fetchGoals);
     const organizedDefaultGoals = {};
     allDefaultGoals.forEach((goal) => {
       if (!organizedDefaultGoals[goal.goal_activity]) {
@@ -269,12 +263,11 @@ module.exports.getDefaultGoals = async () => {
 
 module.exports.completeGoalSuccess = async (userGoalID) => {
   try {
-    const updateGoal = `UPDATE user_goal SET user_goal_finalized = 1 WHERE user_goal_id = ${userGoalID}`;
-    await db.queryAsync(updateGoal);
+    await db.queryAsync('UPDATE user_goal SET user_goal_finalized = 1 WHERE user_goal_id = ?', [userGoalID]);
     const updateEgg = 'UPDATE user_egg SET egg_xp = (egg_xp + (SELECT user_goal_points FROM user_goal ' +
-      `WHERE user_goal_id = ${userGoalID})) WHERE egg_hatched = 0 AND user_id = ` +
-      `(SELECT user_id FROM user_goal WHERE user_goal_id = ${userGoalID})`;
-    await db.queryAsync(updateEgg);
+      `WHERE user_goal_id = ?)) WHERE egg_hatched = 0 AND user_id = ` +
+      `(SELECT user_id FROM user_goal WHERE user_goal_id = ?)`;
+    await db.queryAsync(updateEgg, [userGoalID, userGoalID]);
     return 'success';
   } catch (err) {
     throw err;
@@ -283,8 +276,7 @@ module.exports.completeGoalSuccess = async (userGoalID) => {
 
 module.exports.completeGoalFailure = async (userGoalID) => {
   try {
-    const updateGoal = `UPDATE user_goal SET user_goal_finalized = 1 WHERE user_goal_id = ${userGoalID}`;
-    return await db.queryAsync(updateGoal);
+    return await db.queryAsync('UPDATE user_goal SET user_goal_finalized = 1 WHERE user_goal_id = ?', [userGoalID]);
   } catch (err) {
     throw err;
   }
@@ -292,31 +284,31 @@ module.exports.completeGoalFailure = async (userGoalID) => {
 
 const findEggID = async (userID) => {
   const possibleIDs = await db.queryAsync('SELECT egg_id from egg WHERE egg_id NOT IN ' +
-    `(SELECT egg_id FROM user_egg WHERE user_id=${userID})`);
+    '(SELECT egg_id FROM user_egg WHERE user_id=?)', [userID]);
 
-  return possibleIDs[Math.ceil(Math.random() * possibleIDs.length)].egg_id;
+  return possibleIDs[Math.ceil(Math.random() * possibleIDs.length)].egg_id; //a single egg id
 };
 
 module.exports.hatchEgg = async (userEggID, id, nextXP) => {
   try {
     const userID = await getRightID(id);
-    const hatchEgg = `UPDATE user_egg SET egg_hatched = 1 WHERE user_egg_id = '${userEggID}';`;
-
-    const newSquaddie = 'INSERT INTO user_monster (user_id, monster_id, user_monster_yard) VALUES ' +
-      `('${userID}', (SELECT egg_id FROM user_egg WHERE user_egg_id = '${userEggID}'), 1);`;
-
     const eggID = await findEggID(userID);
 
+    const hatchEgg = `UPDATE user_egg SET egg_hatched = 1 WHERE user_egg_id = ?`;
+
+    const newSquaddie = 'INSERT INTO user_monster (user_id, monster_id, user_monster_yard) VALUES ' +
+      `(?, (SELECT egg_id FROM user_egg WHERE user_egg_id = ?), 1);`;
+
     const makeNewEgg = 'INSERT INTO user_egg (user_id, egg_id, egg_xp) VALUES ' +
-      `('${userID}', ${eggID}, ${nextXP});`;
+      `(?, ?, ?);`;
 
     const returnSquaddie = 'SELECT user_monster.*, monster.* FROM user_monster INNER JOIN monster ' +
       'ON user_monster.monster_id = monster.monster_id WHERE user_monster.user_monster_id = (SELECT MAX(user_monster_id) FROM user_monster);';
 
     await Promise.all([
-      db.queryAsync(hatchEgg),
-      db.queryAsync(newSquaddie),
-      db.queryAsync(makeNewEgg),
+      db.queryAsync(hatchEgg, [userEggID]),
+      db.queryAsync(newSquaddie, [userID, userEggID]),
+      db.queryAsync(makeNewEgg, [userID, eggID, nextXP]),
     ]);
 
     return await db.queryAsync(returnSquaddie);
@@ -329,8 +321,8 @@ module.exports.hatchEgg = async (userEggID, id, nextXP) => {
 module.exports.getEggInfo = async (id) => {
   try {
     const userID = await getRightID(id);
-    const data = await db.queryAsync(`SELECT * FROM user_egg WHERE user_id='${userID}' AND egg_hatched = 0;`);
-    return data.pop(); // removes array and returns only object
+    const egg = await db.queryAsync('SELECT * FROM user_egg WHERE user_id = ? AND egg_hatched = 0', [userID]);
+    return egg[0];
   } catch (e) {
     throw e;
   }
@@ -339,10 +331,10 @@ module.exports.getEggInfo = async (id) => {
 module.exports.getUserSquaddies = async (id) => {
   try {
     const userID = await getRightID(id);
-    const data = await db.queryAsync('SELECT monster.*, user_monster.* FROM monster INNER JOIN ' +
+    const squaddies = await db.queryAsync('SELECT monster.*, user_monster.* FROM monster INNER JOIN ' +
       'user_monster ON user_monster.monster_id = monster.monster_id WHERE ' +
-      `user_monster.user_id = '${userID}';`);
-    return data;
+      `user_monster.user_id = ?`, [userID]);
+    return squaddies;
   } catch (e) {
     throw e;
   }
@@ -353,7 +345,7 @@ module.exports.getAllSquaddies = async (id) => {
     const userID = await getRightID(id);
     // returns a lsit of all squaddies but with null info for ones a user hasn't yet earned
     const allSquaddies = await db.queryAsync('SELECT * FROM monster');
-    const userSquaddies = await db.queryAsync(`SELECT * FROM user_monster WHERE user_id = ${userID}`);
+    const userSquaddies = await db.queryAsync('SELECT * FROM user_monster WHERE user_id = ?', [userID]);
 
     userSquaddies.forEach((squaddie) => {
       allSquaddies[squaddie.monster_id - 1].user = squaddie;
@@ -367,9 +359,9 @@ module.exports.getAllSquaddies = async (id) => {
 module.exports.newUserLifetimeDistance = async (id, distance) => {
   try {
     const userID = await getRightID(id);
-    const updateGoals = `UPDATE user_goal SET user_goal_current = ${distance} ` +
-      `WHERE user_id = '${userID}' AND goal_id > 0 AND goal_id < 7 AND user_goal_concluded = 0`;
-    return await db.queryAsync(updateGoals);
+    const updateGoals = 'UPDATE user_goal SET user_goal_current = ? ' +
+      'WHERE user_id = ? AND goal_id > 0 AND goal_id < 7 AND user_goal_concluded = 0';
+    return await db.queryAsync(updateGoals, [distance, userID]);
   } catch (err) {
     throw (err);
   }
@@ -378,9 +370,9 @@ module.exports.newUserLifetimeDistance = async (id, distance) => {
 module.exports.newUserLifetimeSteps = async (id, steps) => {
   try {
     const userID = await getRightID(id);
-    const updateGoals = `UPDATE user_goal SET user_goal_current = ${steps} ` +
-      `WHERE user_id = '${userID}' AND goal_id > 6 AND goal_id < 13 AND user_goal_concluded = 0`;
-    return await db.queryAsync(updateGoals);
+    const updateGoals = `UPDATE user_goal SET user_goal_current = ? ` +
+      `WHERE user_id = ? AND goal_id > 6 AND goal_id < 13 AND user_goal_concluded = 0`;
+    return await db.queryAsync(updateGoals, [steps, userID]);
   } catch (err) {
     throw (err);
   }
@@ -389,9 +381,9 @@ module.exports.newUserLifetimeSteps = async (id, steps) => {
 module.exports.newUserLifetimeFloors = async (id, floors) => {
   try {
     const userID = await getRightID(id);
-    const updateGoals = `UPDATE user_goal SET user_goal_current = ${floors} ` +
-      `WHERE user_id = '${userID}' AND goal_id > 12 AND goal_id < 19 AND user_goal_concluded = 0`;
-    return await db.queryAsync(updateGoals);
+    const updateGoals = 'UPDATE user_goal SET user_goal_current = ? ' +
+      'WHERE user_id = ? AND goal_id > 12 AND goal_id < 19 AND user_goal_concluded = 0';
+    return await db.queryAsync(updateGoals, floors, userID);
   } catch (err) {
     throw (err);
   }
@@ -399,39 +391,38 @@ module.exports.newUserLifetimeFloors = async (id, floors) => {
 
 module.exports.updateCustomGoalProgress = async (goalID, newCurrent) => {
   try {
-    const updateGoal = `UPDATE user_goal SET user_goal_current = ${newCurrent} + user_goal_current ` +
-      `WHERE user_goal_id = ${goalID}`;
-    await db.queryAsync(updateGoal);
+    const updateGoal = 'UPDATE user_goal SET user_goal_current = ? + user_goal_current WHERE user_goal_id = ?';
+    await db.queryAsync(updateGoal, [newCurrent, goal]);
     return;
   } catch (err) {
     throw (err);
   }
 };
 
-module.exports.updatePushNotificationsToFalse = async userID => {
+module.exports.updatePushNotificationsToFalse = async (userID) => {
   try {
-    const updatePushNotificationsToFalse = `UPDATE user SET notified_of_push_notifications = 1 WHERE user_id = ${userID}`;
-    await db.queryAsync(updatePushNotificationsToFalse);
+    const updatePushNotificationsToFalse = 'UPDATE user SET notified_of_push_notifications = 1 WHERE user_id = ?';
+    await db.queryAsync(updatePushNotificationsToFalse, [userID]);
   } catch (err) {
     throw (err);
   }
-}
+};
 
-module.exports.updatePushNotificationsToTrue = async userID => {
+module.exports.updatePushNotificationsToTrue = async (userID) => {
   try {
     const updatePushNotificationsToTrue = 'UPDATE user SET notified_of_push_notifications = 1, ' +
-    `wants_push_notifications = 1 WHERE user_id = ${userID}`;
-    await db.queryAsync(updatePushNotificationsToTrue);
+    'wants_push_notifications = 1 WHERE user_id = ?';
+    await db.queryAsync(updatePushNotificationsToTrue, [userID]);
   } catch (err) {
     throw (err);
   }
-}
+};
 
-module.exports.unsubscribeFromPushNotifications = async userID => {
+module.exports.unsubscribeFromPushNotifications = async (userID) => {
   try {
     const unsubscribeFromPushNotifications = 'UPDATE user SET wants_push_notifications = 0, ' +
-    `unsubscribed_from_notifications = 1 WHERE user_id = ${userID}`;
-    await db.queryAsync(unsubscribeFromPushNotifications);
+    'unsubscribed_from_notifications = 1 WHERE user_id = ?';
+    await db.queryAsync(unsubscribeFromPushNotifications, [userID]);
   } catch (err) {
     throw (err);
   }
@@ -441,12 +432,10 @@ module.exports.updateGoalStatuses = async () => {
   const markDoneGoals = 'UPDATE user_goal SET user_goal_success = 1, user_goal_concluded = 1 ' +
     'WHERE user_goal_target <= user_goal_current';
   const markExpiredGoals = 'UPDATE user_goal SET user_goal_concluded = 1 WHERE user_goal_end_date < (utc_timestamp());';
-  (async function updateGoals() {
-    await Promise.all([
-      db.queryAsync(markDoneGoals),
-      db.queryAsync(markExpiredGoals),
-    ]);
-  }());
+  await Promise.all([
+    db.queryAsync(markDoneGoals),
+    db.queryAsync(markExpiredGoals),
+  ]);
 };
 
 module.exports.getUserDeets = async (id) => {
@@ -493,8 +482,8 @@ module.exports.getUserDeets = async (id) => {
   };
 
   try {
-    const getUserGoals = `SELECT user_goal.*, goal.* FROM user_goal INNER JOIN goal ON goal.goal_id = user_goal.goal_id WHERE user_id = '${userID}'`;
-    const userGoals = await db.queryAsync(getUserGoals);
+    const getUserGoals = `SELECT user_goal.*, goal.* FROM user_goal INNER JOIN goal ON goal.goal_id = user_goal.goal_id WHERE user_id = ?`;
+    const userGoals = await db.queryAsync(getUserGoals, [userID]);
 
     const getAllGoals = 'SELECT user_goal.*, goal.* FROM user_goal INNER JOIN goal ON goal.goal_id = user_goal.goal_id';
     const allGoals = await db.queryAsync(getAllGoals);
@@ -508,7 +497,7 @@ module.exports.getUserDeets = async (id) => {
 module.exports.getYardSquaddiesByID = async (id) => {
   try {
     const userID = await getRightID(id);
-    return await db.queryAsync(`SELECT * FROM user_monster INNER JOIN monster ON monster.monster_id = user_monster.monster_id WHERE user_id = ${userID} AND user_monster_yard = 1`);
+    return await db.queryAsync('SELECT * FROM user_monster INNER JOIN monster ON monster.monster_id = user_monster.monster_id WHERE user_id = ? AND user_monster_yard = 1', [userID]);
   } catch (err) {
     throw new Error('get yardsquaddies DB error');
   }
@@ -516,8 +505,8 @@ module.exports.getYardSquaddiesByID = async (id) => {
 
 module.exports.updateYardSquaddie = async (userMonsterID) => {
   try {
-    const query = `UPDATE user_monster SET user_monster_yard = !user_monster_yard WHERE user_monster_id = '${userMonsterID}'`;
-    return await db.queryAsync(query);
+    const toggleYard = 'UPDATE user_monster SET user_monster_yard = !user_monster_yard WHERE user_monster_id = ?';
+    return await db.queryAsync(toggleYard, [userMonsterID]);
   } catch (err) {
     throw new Error('error updating yardsquaddie');
   }
@@ -526,7 +515,7 @@ module.exports.updateYardSquaddie = async (userMonsterID) => {
 
 module.exports.renameSquaddie = async (userMonsterID, newName) => {
   try {
-    return await db.queryAsync(`UPDATE user_monster SET user_monster_new_name = '${newName}' WHERE user_monster_id = ${userMonsterID}`);
+    return await db.queryAsync('UPDATE user_monster SET user_monster_new_name = ? WHERE user_monster_id = ?', [newName, userMonsterID]);
   } catch (err) {
     throw new Error('error renaming squaddie');
   }
@@ -534,7 +523,7 @@ module.exports.renameSquaddie = async (userMonsterID, newName) => {
 
 module.exports.saveSquaddiePosition = async (userMonsterPosition) => {
   try {
-    return await db.queryAsync(`UPDATE user_monster SET user_monster_xcoord = ${userMonsterPosition.x}, user_monster_ycoord = ${userMonsterPosition.y} WHERE user_monster_id = ${userMonsterPosition.id}`);
+    return await db.queryAsync('UPDATE user_monster SET user_monster_xcoord = ?, user_monster_ycoord = ? WHERE user_monster_id = ?', [userMonsterPosition.x, userMonsterPosition.y, userMonsterPosition.id]);
   } catch (err) {
     throw new Error('Error saving Squaddie position');
   }
@@ -542,8 +531,8 @@ module.exports.saveSquaddiePosition = async (userMonsterPosition) => {
 
 module.exports.addXPtoMonster = async (monID, xp) => {
   try {
-    return await db.queryAsync(`UPDATE user_monster SET user_monster_current_xp = user_monster_current_xp + ${xp} ` +
-      `WHERE user_monster_id = ${monID};`);
+    return await db.queryAsync(`UPDATE user_monster SET user_monster_current_xp = user_monster_current_xp + ? ` +
+      `WHERE user_monster_id = ?`, [xp, monID]);
   } catch (err) {
     console.log(err);
     throw new Error('ERR adding XP');
@@ -553,10 +542,8 @@ module.exports.addXPtoMonster = async (monID, xp) => {
 module.exports.levelUp = async (monID) => {
   try {
     return await db.queryAsync('UPDATE user_monster SET user_monster_level = user_monster_level + 1, ' +
-      'user_monster_hp = user_monster_hp + 5, ' +
-      'user_monster_attack = user_monster_attack + 1, ' +
-      'user_monster_defense = user_monster_defense + 1 ' +
-      `WHERE user_monster_id = ${monID};`);
+      'user_monster_hp = user_monster_hp + 5, user_monster_attack = user_monster_attack + 1, ' +
+      'user_monster_defense = user_monster_defense + 1 WHERE user_monster_id = ?', [monID]);
   } catch (err) {
     console.log(err);
     throw new Error('level up err');
